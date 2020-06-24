3a80f646a7605fe62a02d5e0d98bcd817f04672f
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-06-24 15:19:03 +0700

    Change icon

--- app/src/main/java/com/rujirakongsomran/kt_checkinternetconnection/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_checkinternetconnection/MainActivity.kt
@@ -21,10 +21,10 @@ class MainActivity : AppCompatActivity() {
 
         // Let's check the connection and display the info in our layout
         if (isConnected) {
-            ivImageStatus.setImageResource(R.drawable.ic_wifi_connection)
+            ivImageStatus.setImageResource(R.drawable.ic_wifi_connect)
             tvStatus.text = "You are connected to the internet"
         }else{
-            ivImageStatus.setImageResource(R.drawable.ic_wifi_disconnect)
+            ivImageStatus.setImageResource(R.drawable.ic_wifi_dis)
             tvStatus.text = "You are not connected to the internet"
         }
 
@@ -34,10 +34,10 @@ class MainActivity : AppCompatActivity() {
             val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
 
             if (isConnected) {
-                ivImageStatus.setImageResource(R.drawable.ic_wifi_connection)
+                ivImageStatus.setImageResource(R.drawable.ic_wifi_connect)
                 tvStatus.text = "You are connected to the internet"
             }else{
-                ivImageStatus.setImageResource(R.drawable.ic_wifi_disconnect)
+                ivImageStatus.setImageResource(R.drawable.ic_wifi_dis)
                 tvStatus.text = "You are not connected to the internet"
             }
         }
