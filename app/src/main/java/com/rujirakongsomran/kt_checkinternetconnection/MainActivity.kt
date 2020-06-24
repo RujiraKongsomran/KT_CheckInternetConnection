bff895a196b23b1df128944906262134ac0172eb
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-06-24 15:12:49 +0700

    Create connection manager and check the connection to display info in layout

--- app/src/main/java/com/rujirakongsomran/kt_checkinternetconnection/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_checkinternetconnection/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.rujirakongsomran.kt_checkinternetconnection
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Let's create our connection manager
+        val connectionManager: ConnectivityManager =
+            this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = connectionManager.activeNetworkInfo
+        val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
+
+        // Let's check the connection and display the info in our layout
+        if (isConnected) {
+            ivImageStatus.setImageResource(R.drawable.ic_wifi_connection)
+            tvStatus.text = "You are connected to the internet"
+        }else{
+            ivImageStatus.setImageResource(R.drawable.ic_wifi_disconnect)
+            tvStatus.text = "You are not connected to the internet"
+        }
+
+        // Let's create the button event function
+        btnCheck.setOnClickListener {
+            val activeNetwork: NetworkInfo? = connectionManager.activeNetworkInfo
+            val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
+
+            if (isConnected) {
+                ivImageStatus.setImageResource(R.drawable.ic_wifi_connection)
+                tvStatus.text = "You are connected to the internet"
+            }else{
+                ivImageStatus.setImageResource(R.drawable.ic_wifi_disconnect)
+                tvStatus.text = "You are not connected to the internet"
+            }
+        }
     }
 }
